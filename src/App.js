8e992565fe3962e8cb1be30437f7499306db952e
Gonpassa <Gonzalopassa@hotmail.com>
2023-05-10 15:27:06 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react'
 import TopBar from './components/TopBar';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Activities from './components/Activities';
+import Booking from './components/Booking';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <TopBar />
       <Navbar />
       <Hero />
+      <Activities />
+      <Booking />
+      <Gallery />
+      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-        <img src="" alt="" />
+    <div className='w-full h-[90vh]'>
+        <img
+        src='https://images.unsplash.com/photo-1505228395891-9a51e7e86bf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3433&q=80'
+        alt='/'
+        className='w-full h-full object-cover'
+        />
+        <div className='max-w-[1140px] m-auto'>
+            <div className='absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-white p-4 '>
+                <h1 className='font-bold text-4xl'> Find Your Special Trip</h1>
+                <h2 className='font-bold text-4xl italic'>With Weekaway</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ducimus repudiandae, numquam exercitationem sed soluta earum alias quae obcaecati commodi esse quas optio nihil voluptates atque recusandae nam inventore magnam?</p>
+            </div>
+        </div>
     </div>
+
   )
 }
 
